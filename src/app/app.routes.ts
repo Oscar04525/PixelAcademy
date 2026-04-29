bce3166b8616fe3881da6b85b55465fda19c8db2
Oscar <oscarlangaterol04@digitechfp.com>
2026-04-29 09:32:24 +0200

    Semana 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { DetalleCursoComponent } from './pages/detalle-curso/detalle-curso.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'course/:id', component: DetalleCursoComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/detalle-curso/detalle-curso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-curso',
+  imports: [],
+  templateUrl: './detalle-curso.component.html',
+  styleUrl: './detalle-curso.component.css'
+})
+export class DetalleCursoComponent {
+
+}
