c30123c3fb5df0195de495cbf4c74a9549f64f4b
Alejandro Martínez <alej.mar.and.267@gmail.com>
2026-05-05 14:12:50 +0200

    arreglos de fallos varios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
-import { LoginComponent } from './pages/login/login.component'; // Corregido: Sin /auth/
-import { RegisterComponent } from './pages/register/register.component'; // Corregido: Sin /auth/
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { DetalleCursoComponent } from './pages/detalle-curso/detalle-curso.component'; // Según tu lista de archivos
+import { DetalleCursoComponent } from './pages/detalle-curso/detalle-curso.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -15,6 +16,7 @@ export const routes: Routes = [
   { path: 'detalle-curso/:id', component: DetalleCursoComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutUsComponent },
