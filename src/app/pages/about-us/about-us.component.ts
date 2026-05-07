453a0841c60d3904e7ef5c25d1e593c61d24e714
Alejandro Martínez <alej.mar.and.267@gmail.com>
2026-05-07 09:24:20 +0200

    cambios en Sobre Nosotros y Contacto

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
-export class AboutUsComponent {
-
-}
+export class AboutUsComponent {}
