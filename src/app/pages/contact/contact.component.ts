7a35955482991ca2b62aa785980044bce9f89bc2
Oscar04525 <oscarlanga04@digitechfp.com>
2026-05-12 19:34:14 +0200

    boton de enviar mensaje mejorado

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css' // Asegúrate de que esta ruta sea correcta si tienes CSS
+  styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  // Aquí puedes añadir lógica de formulario en el futuro
-  enviarMensaje() {
-    alert('Mensaje enviado. ¡Nos pondremos en contacto contigo!');
+  loading = false;
+  successMessage = '';
+  // ESTO ES LO QUE NECESITA EL BOTÓN PARA NO DAR ERROR
+  contactForm = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    apellidos: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    asunto: new FormControl('Pagos y facturación'),
+    mensaje: new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+
+      Swal.fire({
+        title: '¡Sistemas Sincronizados!',
+        text: 'Tu mensaje ha cruzado el ciberespacio con éxito. 🚀',
+        icon: 'success',
+        confirmButtonColor: '#00e5ff',
+        background: '#071126',
+        color: '#fff'
+      });
+      this.contactForm.reset();
+    }, 1500);
   }
 }
